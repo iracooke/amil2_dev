5948753b8e44b46deaa7b3a88ccb5f7b8d159d9d
Ira Cooke <iracooke@gmail.com>
2025-04-25 11:17:21 +1000

    Reorganise plotting code, improve legends, labels and overall consistency

--- plot_genes.R
+++ plot_genes.R
@@ -1,3 +1,40 @@
+scatterplot_theme <- function(){
+  theme(
+    axis.text.x = element_text(angle=70,vjust = 0.5, hjust=0.5),
+    panel.background = element_rect(fill = "white",
+                                    colour = "grey88",
+                                    size = 0.5, linetype = "solid"),
+    panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                    colour = "grey88"), 
+    panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                    colour = "grey88"),
+    panel.border = element_rect(color = "black", fill = NA),
+    
+    #Font sizes
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text = element_text(size = 15),
+    strip.text.x = element_text(size = 15),
+    legend.title=element_text(size=15), 
+    legend.text=element_text(size=15)
+    
+  ) 
+}
+
+common_scatterplot_layers <- function(p){
+  p + xlab("Developmental stage") +
+    # Add axis labels
+    ylab("Log2 gene expression") +
+
+    scale_color_manual(values = c("B"="red3", "A"="green4"), name="Cross") + 
+    scale_shape_manual(values = c(16,17),name="Cross") +
+
+    # Add dashed line as indicator for settlement induction
+    geom_vline(xintercept="133hpf", colour ="grey44",linetype = "dashed") +
+    facet_wrap(~gene)
+}
+
+
 plot_genes <- function(fd,input){
   
   num_genes <- fd$gene %>% n_distinct()
@@ -19,66 +56,22 @@ plot_genes <- function(fd,input){
   if ( num_genes < input$num_hm_genes ){
     p_type <- "line_scatter"
     if ( input$use_average){
-      p <- fd %>% group_by(gene,time_label,time) %>% 
+      p <- fd %>% group_by(gene,time_label,time,cross) %>% 
         summarise(vst=mean(vst,na.rm = TRUE)) %>% 
         ungroup() %>% 
         ggplot(aes(x=reorder(time_label,time),y=vst)) + 
-        geom_point(size = 2.5) + 
-        # Add dashed line as indicator for settlement induction
-        geom_vline(xintercept="133hpf", colour ="grey44",linetype = "dashed")+
-        # Add axis labels
-        xlab("Developmental stages") +
-        ylab("Normalised gene expression \n (Variance stabilising transformations)")+
-        geom_line(aes(group=gene)) +
-        theme(
-          axis.text.x = element_text(angle=70,vjust = 0.5, hjust=0.5),
-          panel.background = element_rect(fill = "white",
-                                          colour = "grey88",
-                                          size = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                          colour = "grey88"), 
-          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "grey88"),
-          panel.border = element_rect(color = "black", fill = NA),
-          #Font sizes
-          axis.title.x = element_text(size = 20),
-          axis.title.y = element_text(size = 20),
-          axis.text = element_text(size = 15),
-          strip.text.x = element_text(size = 15)
-        
-        ) +  
-        facet_wrap(~gene)        
+        geom_point(size = 2.5,aes(color=cross)) +
+        geom_line(aes(group=cross,color=cross)) +
+        scatterplot_theme() 
+      p <- p %>% common_scatterplot_layers()
     } else {
-      p <- ggplot(fd,aes(x=reorder(time_label,time),y=vst)) + 
+      p <-  fd %>% group_by(gene,time_label,time,cross) %>% 
+        mutate(mean_vst = mean(vst,na.rm = TRUE)) %>% 
+        ggplot(aes(x=reorder(time_label,time),y=vst)) + 
         geom_point(aes(color=cross, shape=cross),size = 2.5) + 
-        geom_vline(xintercept="133hpf", colour ="grey44",linetype = "dashed")+
-        theme(
-          axis.text.x = element_text(angle=70,vjust = 0.5, hjust=0.5),
-          panel.background = element_rect(fill = "white",
-                                          colour = "grey88",
-                                          size = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                          colour = "grey88"), 
-          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "grey88"),
-          panel.border = element_rect(color = "black", fill = NA),
-          
-          #Font sizes
-          axis.title.x = element_text(size = 20),
-          axis.title.y = element_text(size = 20),
-          axis.text = element_text(size = 15),
-          strip.text.x = element_text(size = 15),
-          legend.title=element_text(size=15), 
-          legend.text=element_text(size=15)
-          
-        ) +  
-        # Add axis labels
-        xlab("Developmental stages") +
-        ylab("Normalised gene expression \n (Variance stabilising transformations)")+
-        # Manually change colours
-        scale_color_manual(values = c("B"="red3", "A"="green4"), name="Cross")+ 
-        scale_shape_manual(values = c(16,17),name="Cross")+
-        facet_wrap(~gene)
+        geom_line(aes(y=mean_vst,group=cross,color=cross)) +
+        scatterplot_theme()
+      p <- p %>% common_scatterplot_layers()
     }
     
   } else {
@@ -108,7 +101,7 @@ plot_genes <- function(fd,input){
         pivot_wider(names_from = sample,values_from = vst,id_cols = gene)
       
       hm_matrix <- hmd %>% column_to_rownames(var="gene") %>% as.matrix()
-      
+
       hm_centered <- hm_matrix %>% t() %>% scale(scale=FALSE) %>% t()
       
       condition_info <- fd %>% select(sample,time,cross) %>% unique()
@@ -117,10 +110,14 @@ plot_genes <- function(fd,input){
       
     }
     
-    
+#    browser()
     p <- Heatmap(hm_centered[,column_order],
                  cluster_columns = FALSE, 
-                 col=rev(brewer.pal(n = 11, name = "RdBu"))
+                 col=rev(brewer.pal(n = 11, name = "RdBu")),
+                 column_title = "Developmental stage",
+                 row_title = "Gene",
+                 show_row_names = (nrow(hm_centered)<100),
+                 heatmap_legend_param = list(title = "log2 fold change",direction = "horizontal")
                  )
   }
   list(plot = p, type = p_type)
